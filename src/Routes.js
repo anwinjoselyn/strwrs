638e8284664f0638027f38d8d2cdd2d5034013b3
Anwin Joselyn <anwinj@gmail.com>
2021-02-26 17:23:56 +0530

    added code splitting

--- src/Routes.js
+++ src/Routes.js
@@ -1,31 +1,42 @@
 import React from "react";
 import { Route, Switch } from "react-router-dom";
 
+//For simple code splitting
+//Since our current APP is simple not using loadable (or other) library for advanced code splitting
+import asyncComponent from "./components/AsyncComponent";
+
+//Different route types
+import AppliedRoute from "./components/Routes/AppliedRoute";
 import AuthenticatedRoute from "./components/Routes/AuthenticatedRoute";
 import UnauthenticatedRoute from "./components/Routes/UnauthenticatedRoute";
 
-import Home from "./containers/Home/Home";
-import Login from "./containers/Login/Login";
-import Planets from "./containers/Home/Planets";
-import NotFound from "./containers/NotFound/NotFound";
+//Load required components dynamically and when required
+const AsyncHome = asyncComponent(() => import("./containers/Home/Home"));
+const AsyncLogin = asyncComponent(() => import("./containers/Login/Login"));
+const AsyncPlanets = asyncComponent(() => import("./containers/Home/Planets"));
+const AsyncNotFound = asyncComponent(() =>
+  import("./containers/NotFound/NotFound")
+);
 
-export default function Routes() {
-  return (
-    <Switch>
-      <Route exact path="/">
-        <Home />
-      </Route>
-      <UnauthenticatedRoute exact path="/login">
-        <Login />
-      </UnauthenticatedRoute>
-      <AuthenticatedRoute exact path="/planets/:id">
-        <Planets />
-      </AuthenticatedRoute>
+const Routes = ({ childProps }) => (
+  <Switch>
+    <AppliedRoute path="/" exact component={AsyncHome} props={childProps} />
+    <UnauthenticatedRoute
+      path="/login"
+      exact
+      component={AsyncLogin}
+      props={childProps}
+    />
+    <AuthenticatedRoute
+      path="/planets/:id"
+      exact
+      component={AsyncPlanets}
+      props={childProps}
+    />
 
-      {/* Catch all unmatched routes */}
-      <Route>
-        <NotFound />
-      </Route>
-    </Switch>
-  );
-}
+    {/* Catch all unmatched routes */}
+    <Route component={AsyncNotFound} />
+  </Switch>
+);
+
+export default Routes;
